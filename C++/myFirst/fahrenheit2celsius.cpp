7515a1380213eef335cc415a0c53cef7e4f804a1
fj <parsafiruzjaei@gmail.com>
2025-12-30 17:23:26 +0330

    fahrenheit_2_celsius_Updated

--- C++/myFirst/fahrenheit2celsius.cpp
+++ C++/myFirst/fahrenheit2celsius.cpp
@@ -3,16 +3,34 @@
 
 using namespace std;
 
-void f2c(float f) {
+float f2c(float f) {
 
-    float C = (f - 32) / 1.8;
-    cout << "fahrenheit: " << f << " celsius: " << C;
-
+    return (f - 32) / 1.8;
 }
 
 int main(){
+    bool run = true;
+    while (run){
+        float f;
+
+        cout << "Enter fahrenheit: ";
+        cin >> f;
+
+        cout << "fahrenheit: " << f
+        << "celsius: " << f2c(f) << endl;
+
+        string again;
+
+        cout << "Do you want to convert again? (y/n): ";
+        cin >> again;
+
+        if (again == "y" || again == "Y" || again == "yes" || again == "Yes"){
+
+            run = true;
+
+            } else { run = false;}
+    }
 
 
-    f2c(92);
     return 0;
 }
